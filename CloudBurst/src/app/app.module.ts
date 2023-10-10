d17aec94af0de5a73a4f891cbb27e5209b6a4c7b
jagadeesh0413 <jagadeeshwaran04012003@gmail.com>
2023-10-10 23:17:57 +0530

    Added home and dashboard components in CloudBurst

--- CloudBurst/src/app/app.module.ts
+++ CloudBurst/src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { RouterModule, Routes } from '@angular/router';
 
+const routes: Routes=[
+  {path:'',component:HomeComponent},
+  {path:'dashboard',component:DashboardComponent}
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
